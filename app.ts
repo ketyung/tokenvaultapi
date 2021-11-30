08a934220573060431b7d6ddd19ada93ed2b50dd
techchee <ketyung@techchee.com>
2021-11-30 11:20:45 +0800

    first commit

---
+++ app.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { getPayer } from './utils/utils';
+import {createTokenVaultAndSetAuthority, TokenVaultPair} from './utils/funcs';
+import {Keypair} from '@solana/web3.js';
+
+const app = express();
+const port = 3000;
+
+interface Wallet {
+
+    publicKey : string; 
+}
+
+const pubKeyResp = async (_request : Request, response : Response, _next : NextFunction) => {
+
+     const res = await getPayer();
+     
+     if ( res instanceof Keypair){
+
+        let wallet  : Wallet = { publicKey: res.publicKey.toBase58()};
+
+        response.status(200).json(  wallet );
+
+     }
+     else {
+
+        response.status(200);
+
+     }
+}
+
+const createResp =  async (request : Request, response : Response, _next : NextFunction) => {
+
+     let tk = request.query.token;
+
+     console.log("tk::", tk);
+
+     const res = await createTokenVaultAndSetAuthority(tk.toString());
+
+     response.status(200).json(  res );
+
+}
+
+//app.get('/publicKey', pubKeyResp);
+
+app.get('/token', createResp);
+
+
+app.listen(port, () => {
+  console.log(`Token vault API is running on port ${port}.`);
+});
---
+++ utils/funcs.ts
@@ -0,0 +1,122 @@
+import * as web3 from '@solana/web3.js';
+// import spl token package, yarn add or npm install it if you don't have 
+import * as splToken from "@solana/spl-token";
+import { getRpcUrl , getPayer} from './utils';
+let util= require('util');
+
+// you'll need the associated token program ID
+const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID: web3.PublicKey = new web3.PublicKey(
+        'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',);
+
+// the program id of my Rust smart contract
+export const programId : web3.PublicKey = new web3.PublicKey("5E4MATRbVPrHsHKWyYUu1HvLS5dhhajuETdSCeuqDf64");
+
+
+let connection : web3.Connection; 
+
+
+export interface TokenVaultPair {
+
+    mint : string;
+
+    account : string;
+}
+
+export async function establishConnection(): Promise<void> {
+    const rpcUrl = await getRpcUrl();
+    connection = new web3.Connection(rpcUrl, 'confirmed');
+    const version = await connection.getVersion();
+    console.log('Connection to cluster established:', rpcUrl, version);
+}
+
+// this function allows a wallet to find an associated token account 
+// for a token 
+export async function findAssociatedTokenAddress(walletAddress: web3.PublicKey, 
+tokenMintAddress: web3.PublicKey): Promise<web3.PublicKey> {
+    return (await web3.PublicKey.findProgramAddress(
+        [
+            walletAddress.toBuffer(),
+            splToken.TOKEN_PROGRAM_ID.toBuffer(),
+            tokenMintAddress.toBuffer(),
+        ],
+        SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+    ))[0];
+}
+
+
+      
+
+export async function createTokenVaultAndSetAuthority(nftMintStr : String) : Promise <TokenVaultPair>{
+
+    establishConnection();
+
+    let payer = await getPayer();
+
+    let walletKey = payer.publicKey;
+
+    let nftMint = new web3.PublicKey(nftMintStr);
+
+
+    let nftVaultAccount = await findAssociatedTokenAddress(walletKey, nftMint );
+
+       
+    console.log("The associated token account is :", nftVaultAccount.toBase58());
+
+    const allTxs = new web3.Transaction();
+        
+       // check if the token account exists 
+    if ( await connection.getAccountInfo(nftVaultAccount) == null ){
+
+          // if not, add and instruction of creating the associated token account to
+          // web3 transaction to create it 
+            allTxs.add(
+
+                splToken.Token.createAssociatedTokenAccountInstruction(
+                    SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID, splToken.TOKEN_PROGRAM_ID,
+                    nftMint,nftVaultAccount, walletKey, walletKey),
+    
+            );
+     }
+     else {
+
+        console.log("Token Account already exists!");
+
+        let t : TokenVaultPair = ({ mint : nftMint.toBase58(), account : nftVaultAccount.toBase58()});        
+        return t; 
+     }
+
+     // create a PDA derived from token mint
+   
+     //new util.TextEncoder().encode(nftMintStr)
+
+     let pdaAccount = await web3.PublicKey.findProgramAddress(
+      [new util.TextEncoder().encode("xxxx.x")], programId);
+
+     console.log("The PDA :", pdaAccount[0]);
+
+     // create the set authority instruction
+     // to change the authority to the PDA
+     let setAuthIns = splToken.Token.createSetAuthorityInstruction(
+         new web3.PublicKey(splToken.TOKEN_PROGRAM_ID), 
+        nftVaultAccount, pdaAccount[0], "AccountOwner", walletKey, [payer]);
+     
+
+    allTxs.add( setAuthIns );
+
+    
+    // send the tx 
+    web3.sendAndConfirmTransaction( connection, allTxs, [payer]);
+    
+    // continue here to save the token mint and 
+    // token account to a DB
+
+    // and return the JSON containing the NFT mint and the NFT vault account pair
+    
+    // And also program the part to return the pair 
+    // by API GET call
+
+    let t : TokenVaultPair = ({ mint : nftMint.toBase58(), account : nftVaultAccount.toBase58()});        
+    return t; 
+
+
+}
